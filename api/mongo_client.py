ee6f77f80ca9aba0d452f4a48f2989ddffdaa57b
Alina Harahulia <lisha2512@gmail.com>
2023-01-11 14:22:10 +0200

    added full functionality of post and get requests

---
+++ api/mongo_client.py
@@ -0,0 +1,28 @@
+"""MONGO CLIIENT"""
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path="./.env.local")
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+
+mongo_client = MongoClient(
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT
+)
+
+
+def insert_test_document():
+
+    """"Inserts sample document to the the test_coolection db"""
+    db = mongo_client.test
+    db.test_collection = db.test_collection
+    res = db.test_collection.insert_one({"name": "Alina", "student": True})
+    print(res)
