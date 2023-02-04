1d15fa71540cc2cb13cd16212a5cd17587ea0758
Alina Harahulia <lisha2512@gmail.com>
2023-02-04 14:42:49 +0100

    added toast notification

--- frontend/Images_gallery/src/App.js
+++ frontend/Images_gallery/src/App.js
@@ -1,10 +1,12 @@
+import { useState, useEffect, React } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import Header from "./components/Header";
 import Search from "./components/Search";
-import { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
-import { Container, Row, Col,} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome";
 import Spinner from "./components/Spinner";
 
@@ -20,10 +22,13 @@ const App = () => {
       try {
         const res = await axios.get(`${API_URL}/images`);
         console.log(res.data);
+
         setImages(res.data || []);
         setLoading(false);
+        toast.success("Saved images downloaded ");
       } catch (error) {
         console.log(error);
+        toast.error(error.message);
       }
     };
     getSavedImages();
@@ -34,20 +39,28 @@ const App = () => {
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}`);
       setImages([{ ...res.data, title: word }, ...images]);
+      toast.info(`New image ${word} was found`);
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
     setWord("");
   };
 
   const handleDeleteImage = async (id) => {
     try {
-      const res = await axios.delete(`${API_URL}/images/${id}>`);
+      const res = await axios.delete(`${API_URL}/images/${id}`);
       if (res.data?.deleted_id) {
+        toast.warn(
+          `Image ${images
+            .find((i) => i.id === id)
+            .title.toUpperCase()} was deleted`
+        );
         setImages(images.filter((image) => image.id !== id));
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -63,6 +76,7 @@ const App = () => {
             image.id === id ? { ...image, saved: true } : image
           )
         );
+        toast.info(`Image ${imageToBeSaved.title} was saved`);
       }
     } catch (error) {
       console.log(error);
@@ -90,7 +104,7 @@ const App = () => {
                       image={image}
                       deleteImage={handleDeleteImage}
                       saveImage={handleSaveImage}
-                    />
+                    ></ImageCard>
                   </Col>
                 ))}
               </Row>
@@ -100,6 +114,7 @@ const App = () => {
           </Container>
         </>
       )}
+      <ToastContainer position="bottom-right"></ToastContainer>
     </div>
   );
 };
